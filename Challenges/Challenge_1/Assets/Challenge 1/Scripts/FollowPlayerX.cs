5542d1e851eddaf4224be383b92165e1811e0f0f
erensome <erensome1@gmail.com>
2023-01-22 13:45:16 +0300

    Challenge 1 completed

---
+++ Challenges/Challenge_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30f,1.5f,10f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // LateUpdate is called after Update method
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
